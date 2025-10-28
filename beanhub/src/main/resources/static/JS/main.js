313eb1873bebd705e7b4d2271b7b0592cb6fd7fd
Alessio Alduina <whiteyes1234@gmail.com>
2025-10-28 15:49:23 +0100

    commit

---
+++ beanhub/src/main/resources/static/JS/main.js
@@ -0,0 +1,21 @@
+
+function toggleLangDropdown() {
+    document.getElementById("lang-dropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+
+    
+    if (!event.target.closest('.dropdown-toggle')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+
+        
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
